6a71405963b1f202e8e49bd2d071bf835f9253d6
Neill Turner <neillwturner@gmail.com>
2015-02-15 19:54:40 +0000

    Update sqs.rb

--- sqs.rb
+++ sqs.rb
@@ -1,6 +1,7 @@
 CloudFormation do
   AWSTemplateFormatVersion("2010-09-09")
 
+# Amazon SQS Queue Snippet
   Resource("MyQueue") do
     Type("AWS::SQS::Queue")
     Property("VisibilityTimeout", "value")
