57f489a2fb41dc2a289e1e51a9967524e8b2a390
Neill Turner <neillwturner@gmail.com>
2015-02-15 19:53:30 +0000

    Update simpledb.rb

--- simpledb.rb
+++ simpledb.rb
@@ -1,6 +1,7 @@
 CloudFormation do
   AWSTemplateFormatVersion("2010-09-09")
 
+# Amazon SimpleDB Domain Resource
   Resource("MySDBDomain") do
     Type("AWS::SDB::Domain")
     Property("Description", "Other than this AWS CloudFormation Description property, SDB Domains have no properties.")
